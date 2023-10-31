27e5e90955e15c3f7d9a1a6af8b7a88f7a8cfea4
Samuel-Amaro <Samuelamaro96746313@gmail.com>
2023-10-31 14:15:04 +0000

    add `database.js` and `.env` files

--- src/app/api/v1/status/route.js
+++ src/app/api/v1/status/route.js
@@ -1,10 +1,13 @@
 import { NextResponse } from "next/server";
+import database from "../../../../../infra/database.js";
 
 /**
  * endpoint /api/status vai retornar a saude do nosso sistema 
  * @param {*} request 
  */
 export async function GET(request) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   //define status code da resposta, é um numero que define se deu tudo certo ou não na requisição, valor 200 sinaliza que deu tudo ok
   return NextResponse.json({message: "Olá, curso.dev."})
 }
